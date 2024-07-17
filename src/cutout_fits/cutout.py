b59c2cf2115cc343406c19bec48c88d2db995de4
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2024-07-17 16:02:00 +0800

    Condense

--- src/cutout_fits/cutout.py
+++ src/cutout_fits/cutout.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 from __future__ import annotations
 
+import argparse
 import os
 from typing import NamedTuple
 
@@ -11,7 +13,7 @@ from astropy.wcs import WCS
 from astropy.wcs.utils import skycoord_to_pixel
 from dotenv import load_dotenv
 
-from cutout_fits.logging import setup_logger
+from cutout_fits.logging import set_verbosity, setup_logger
 
 logger = setup_logger()
 
@@ -312,3 +314,54 @@ def make_cutout(
 
     logger.info("Cutout saved to %s", outfile)
     return cutout_hdulist
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Make a cutout of a FITS file")
+    parser.add_argument("infile", help="Path to input FITS file - can be a remote URL")
+    parser.add_argument("outfile", help="Path to output FITS file")
+    parser.add_argument("ra_deg", type=float, help="Centre RA in degrees")
+    parser.add_argument("dec_deg", type=float, help="Centre Dec in degrees")
+    parser.add_argument("radius_arcmin", type=float, help="Cutout radius in arcminutes")
+    parser.add_argument(
+        "--freq-start",
+        type=float,
+        help="Start frequency in Hz",
+        default=None,
+    )
+    parser.add_argument(
+        "--freq-end",
+        type=float,
+        help="End frequency in Hz",
+        default=None,
+    )
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        action="store_true",
+        help="Overwrite output file if it exists",
+    )
+    parser.add_argument(
+        "-v", "--verbosity", default=0, action="count", help="Increase output verbosity"
+    )
+    args = parser.parse_args()
+
+    set_verbosity(
+        logger=logger,
+        verbosity=args.verbosity,
+    )
+
+    _ = make_cutout(
+        infile=args.infile,
+        outfile=args.outfile,
+        ra_deg=args.ra_deg,
+        dec_deg=args.dec_deg,
+        radius_arcmin=args.radius_arcmin,
+        freq_start_hz=args.freq_start,
+        freq_end_hz=args.freq_end,
+        overwrite=args.overwrite,
+    )
+
+
+if __name__ == "__main__":
+    main()
